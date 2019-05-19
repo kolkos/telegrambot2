c2217de5c5092b178db5dec0b095ab31a4d04e96
Kolkos <antonkolk@gmail.com>
2019-05-19 09:23:26 +0200

    clean

---
+++ src/main/java/nl/kolkos/telegrambot2/Telegrambot2Application.java
@@ -0,0 +1,13 @@
+package nl.kolkos.telegrambot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Telegrambot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Telegrambot2Application.class, args);
+	}
+
+}
---
+++ src/test/java/nl/kolkos/telegrambot2/Telegrambot2ApplicationTests.java
@@ -0,0 +1,16 @@
+package nl.kolkos.telegrambot2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Telegrambot2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
